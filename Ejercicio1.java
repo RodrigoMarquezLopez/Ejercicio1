0f6ba5ff7815802f9acc30cf4e0f2768d6422c5f
RodrigoMarquezLopez <57580250+RodrigoMarquezLopez@users.noreply.github.com>
2020-04-15 21:26:26 -0500

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,12 +1,12 @@
 public class Ejercicio1
 {
-    
+    //iniciamos el metodo con un parametro entero
     public String Serie (int  x){
-     int cont;
+     int cont;//contador que guradara el valor de "x"
      String salida="";
         if(x<0) return String.valueOf(x);
      else{
-         while(x>=1){
+         while(x>=1){//ciclo para generar serie
             cont =x;
             x=x/2;
             if(x%10 > 0 && x!=26 && (x>10 && x%2>0)){
